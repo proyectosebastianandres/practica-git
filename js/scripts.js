ae3c5ae916677c72660cccb10d301fa1784afdbf
andres s <programacionandresserna@gmail.com>
2018-08-17 13:29:21 -0500

    The button was created with id: showMessage, the script.js was linked within index.html and the function was created to show the window with the alert

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+
+//id of the button on variable btn_showMessage
+let btn_showMessage = document.getElementById("showMessage").onclick= showMessage;
+
+function showMessage() {
+    alert("Hello world!");
+}
